db79f4e4fc87412837d2751d915248216e0590a2
myteamwheel <myteamwheel@gmail.com>
2026-08-16 14:48:00 -0400

    Remove unused Patch 14 standard-time cron alias

--- src/app/api/cron/refresh-standard/route.ts
+++
@@ -1,8 +0,0 @@
-import { GET as runPatch14Refresh } from "../refresh/route";
-
-export const maxDuration = 300;
-export const dynamic = "force-dynamic";
-
-export async function GET(request: Request) {
-  return runPatch14Refresh(request);
-}
